3b93524f84eeb3faf5218e1327e310eb00de21ad
fschaefer <farleyschaefer@gmail.com>
2016-09-19 12:01:48 +1200

    Fix formatting.

--- android/Background/CloudScroller.cs
+++ android/Background/CloudScroller.cs
@@ -8,93 +8,93 @@ using System.Collections.Generic;
 
 namespace FallingCatGame.Background
 {
-    public class CloudScroller : IGameLogic
-    {
-        // The number of clouds on screen at a given time, this number is divided by the number of cloud layers.
-        private const int NumberOfClouds = 6;
+	public class CloudScroller : IGameLogic
+	{
+		// The number of clouds on screen at a given time, this number is divided by the number of cloud layers.
+		private const int NumberOfClouds = 6;
 
-        private int _screenWidth;
-        private int _screenHeight;
+		private int _screenWidth;
+		private int _screenHeight;
 
-        // Cloud properties.
-        private Texture2D _cloudTexture;
-        private float _scale;
+		// Cloud properties.
+		private Texture2D _cloudTexture;
+		private float _scale;
 
-        // Scroller properties.
-        private List<List<GameObject>> _clouds;
-        private List<GameObject> _firstLayer;
-        private List<GameObject> _secondLayer;
-        private List<GameObject> _thirdLayer;
+		// Scroller properties.
+		private List<List<GameObject>> _clouds;
+		private List<GameObject> _firstLayer;
+		private List<GameObject> _secondLayer;
+		private List<GameObject> _thirdLayer;
 
-        public CloudScroller(ContentManager content, float scale)
-        {
-            _screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-            _screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+		public CloudScroller(ContentManager content, float scale)
+		{
+			_screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+			_screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
 
-            _firstLayer = new List<GameObject>();
-            _secondLayer = new List<GameObject>();
-            _thirdLayer = new List<GameObject>();
-            _clouds = new List<List<GameObject>>();
+			_firstLayer = new List<GameObject>();
+			_secondLayer = new List<GameObject>();
+			_thirdLayer = new List<GameObject>();
+			_clouds = new List<List<GameObject>>();
 
-            _scale = scale;
-            LoadContent(content);
-            InitializeClouds();
-        }
+			_scale = scale;
+			LoadContent(content);
+			InitializeClouds();
+		}
 
-        private void LoadContent(ContentManager content)
-        {
-            _cloudTexture = content.Load<Texture2D>("Cloud");
-        }
+		private void LoadContent(ContentManager content)
+		{
+			_cloudTexture = content.Load<Texture2D>("Cloud");
+		}
 
-        private void InitializeClouds()
-        {
+		private void InitializeClouds()
+		{
 			int nClouds = NumberOfClouds / 3;
-            Random seed = new Random();
+			Random seed = new Random();
 
-            for (int i = 0; i < nClouds; i++)
-            {
-                _firstLayer.Add(new GameObject(_cloudTexture, _scale, Vector2.Zero, new Vector2(1, 0), seed.Next(100, 200)));
-                _secondLayer.Add(new GameObject(_cloudTexture, _scale * 2, Vector2.Zero, new Vector2(1, 0), seed.Next(50, 100)));
-                _thirdLayer.Add(new GameObject(_cloudTexture, _scale * 3, Vector2.Zero, new Vector2(1, 0),  seed.Next(25, 50)));
-            }
+			for (int i = 0; i < nClouds; i++)
+			{
+				_firstLayer.Add(new GameObject(_cloudTexture, _scale, Vector2.Zero, new Vector2(1, 0), seed.Next(100, 200)));
+				_secondLayer.Add(new GameObject(_cloudTexture, _scale * 2, Vector2.Zero, new Vector2(1, 0), seed.Next(50, 100)));
+				_thirdLayer.Add(new GameObject(_cloudTexture, _scale * 3, Vector2.Zero, new Vector2(1, 0), seed.Next(25, 50)));
+			}
 
-            _clouds.Add(_thirdLayer);
-            _clouds.Add(_secondLayer);
-            _clouds.Add(_firstLayer);
-        }
+			_clouds.Add(_thirdLayer);
+			_clouds.Add(_secondLayer);
+			_clouds.Add(_firstLayer);
+		}
 
-        private Vector2 GetRandomYPosition(Random seed)
-        {
-            return new Vector2(_screenWidth, seed.Next(0, _screenHeight / 2));
-        }
+		private Vector2 GetRandomYPosition(Random seed)
+		{
+			return new Vector2(_screenWidth, seed.Next(0, _screenHeight / 2));
+		}
 
-        public void Update(GameTime gameTime)
-        {
-            Random seed = new Random();
+		public void Update(GameTime gameTime)
+		{
+			Random seed = new Random();
 
-            foreach (List<GameObject> layer in _clouds)
-            {
-                foreach (GameObject cloud in layer)
-                {
-                    if (cloud.Position.X < 0 - cloud.Width)
-                    {
-                        cloud.Position = GetRandomYPosition(seed);
-                    }
+			foreach (List<GameObject> layer in _clouds)
+			{
+				foreach (GameObject cloud in layer)
+				{
+					if (cloud.Position.X < 0 - cloud.Width)
+					{
+						cloud.Position = GetRandomYPosition(seed);
+					}
 
-                    cloud.Position -= cloud.Direction * cloud.Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
-            }
-        }
+					cloud.Position -= cloud.Direction * cloud.Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+				}
+			}
+		}
 
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            foreach (List<GameObject> layer in _clouds)
-            {
-                foreach (GameObject cloud in layer)
-                {
-                    cloud.Draw(spriteBatch);
-                }
-            }
-        }
-    }
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			foreach (List<GameObject> layer in _clouds)
+			{
+				foreach (GameObject cloud in layer)
+				{
+					cloud.Draw(spriteBatch);
+				}
+			}
+		}
+	}
 }
