4902b9a256208f63b4e66644b04c2be4c81138f4
kevbot-git <kamenhinick@gmail.com>
2016-09-19 12:00:31 +1200

    Commiting for branch change

--- android/Control/PlayerControl.cs
+++ android/Control/PlayerControl.cs
@@ -3,22 +3,25 @@ using FallingCatGame.Main;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using FallingCatGame.Drawing;
+using Android.Util;
 
 namespace FallingCatGame.Control
 {
     class PlayerControl
     {
         private static readonly float ACCEL_THRESH = 2.0f;
-        private static readonly float ANIM_TIME = 0.8f;
+        private static readonly float ANIM_TIME = 1f;
 
         public Sprite playerSprite { get; private set; }
 
-        private float centerX;
-        private float leftX;
-        private float rightX;
-        private Movement movement;
-        private Movement prevMovement;
-        GameTime lastChange;
+        private readonly float centerX;
+        private readonly float leftX;
+        private readonly float rightX;
+        private Lane lane;
+        private Lane prevLane;
+        private float lastChangeTime;
+        private float lastChangePos;
+        private bool isMoving;
 
         public PlayerControl(Sprite playerSprite, float centerX, float offset)
         {
@@ -26,52 +29,78 @@ namespace FallingCatGame.Control
             this.centerX = centerX;
             leftX = centerX - offset;
             rightX = centerX + offset;
-            movement = Movement.NONE;
-            prevMovement = Movement.NONE;
+            lane = Lane.CENTER;
+            prevLane = Lane.CENTER;
+            isMoving = false;
         }
 
         public void Update(GameTime gameTime, Vector3 accelerometer)
         {
-            prevMovement = movement;
+            float cT = (float) gameTime.TotalGameTime.TotalSeconds;
+            prevLane = lane;
 
             if (accelerometer.X < -ACCEL_THRESH)
             {
                 // Move right
-                movement = Movement.RIGHT;
+                lane = Lane.RIGHT;
             }
             else if (accelerometer.X > ACCEL_THRESH)
             {
                 // Move left
-                movement = Movement.LEFT;
+                lane = Lane.LEFT;
             }
             else
             {
                 // Re center
-                movement = Movement.NONE;
+                lane = Lane.CENTER;
             }
 
-            if (movement != prevMovement)
+            if (lane != prevLane)
             {
-                lastChange = gameTime;
+                lastChangeTime = cT;
+                lastChangePos = playerSprite.Position.X;
+                Log.Info("ABC123", "lastChangePos: " + lastChangePos);
             }
 
-            switch(movement)
+            switch(lane)
             {
-                case Movement.LEFT:
-                    playerSprite.Position.X = leftX;
+                case Lane.LEFT:
+                    playerSprite.Position = new Vector2(ease(lastChangeTime, cT, ANIM_TIME, centerX, playerSprite.Position.X, leftX), playerSprite.Position.Y);
                     break;
-                case Movement.RIGHT:
 
+                case Lane.RIGHT:
+                    playerSprite.Position = new Vector2(ease(lastChangeTime, cT, ANIM_TIME, centerX, playerSprite.Position.X, rightX), playerSprite.Position.Y);
                     break;
-                case Movement.NONE:
 
+                case Lane.CENTER:
+                    //playerSprite.Position = new Vector2(centerX, playerSprite.Position.Y);
+                    
+                    playerSprite.Position = new Vector2(ease(lastChangeTime, cT, ANIM_TIME, (lastChangePos > centerX) ? rightX : leftX, playerSprite.Position.X, centerX), playerSprite.Position.Y);
                     break;
             }
         }
 
-        private enum Movement
+        private float ease(float startTime, float currentTime, float animTime, float startX, float currentX, float targetX)
         {
-            LEFT = -1, NONE = 0, RIGHT = 1
+            float elapsedTime = currentTime - startTime;
+
+            if (elapsedTime >= animTime)
+            {
+                isMoving = false;
+                return targetX;
+            }
+            else
+            {
+                isMoving = true;
+                return (float)(startX - (((targetX - startX) / 2) * Math.Cos(elapsedTime * Math.PI) / animTime) + (targetX - startX) / 2);
+            }
+        }
+
+        private enum Lane
+        {
+            LEFT = -1, CENTER = 0, RIGHT = 1
+
+            //static float Position { get; set; }
         }
     }
 }
