090e6f75ee4fe4519cdff7625e784ca01e543be9
fschaefer <farleyschaefer@gmail.com>
2016-09-24 22:21:11 +1200

    Add testing framework skeleton.

--- android/Test/Assert.cs
+++ android/Test/Assert.cs
@@ -3,8 +3,51 @@ namespace FallingCatGame
 {
 	public class Assert
 	{
+
 		public Assert()
 		{
+		}
+
+		public bool Equal(object expected, object actual, string message)
+		{
+			if (expected.Equals(actual))
+				return true;
+			// do something with failure..
+			return false;
+		}
+
+		public bool False(bool val, string message)
+		{
+			if (val != false)
+				// do something with failure..
+				return false;
+			return true;
+		}
+
+		public bool True(bool val, string message)
+		{
+			if (val != true)
+				// do something with failure..
+				return false;
+			return true;
+		}
+
+
+		public bool Null(object obj, string message)
+		{
+			if (obj == null)
+				return true;
+			// do something with failure..
+			return false;
+
+		}
+
+		public bool NotNull(object obj, string message)
+		{
+			if (obj != null)
+				return true;
+			// do something with failure..
+			return false;
 		}
 	}
 }
